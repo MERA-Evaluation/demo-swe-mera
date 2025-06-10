51982b2c1378bb5482a09c893e1b5ce719b6a4be
vesokolov <VEvgeSokolov@sberbank.ru>
2025-06-10 21:29:35 +0300

    add: переключение языка

--- src/pages/Description.svelte
+++ src/pages/Description.svelte
@@ -2,26 +2,38 @@
   import { getContext, onDestroy } from 'svelte';
   import TextBlock from '../components/TextBlock.svelte';
   import { firstParagraphText } from '../data/textData';
+  import textData from '../data/textData.json';
 
   const languageStore = getContext('language');
   let lang;
 
-  const unsubscribe = languageStore.subscribe(value => {
-    lang = value
-  })
+  const unsubscribe = languageStore.subscribe((value) => {
+    lang = value;
+  });
 
   onDestroy(unsubscribe);
 </script>
 
 <section>
-  {console.log(lang)}
-  <TextBlock headerBlock="Оглавление">
-    {firstParagraphText}
-  </TextBlock>
-  <TextBlock headerBlock="Оглавление">
-    {firstParagraphText}
-  </TextBlock>
-  <TextBlock headerBlock="Оглавление">
-    {firstParagraphText}
-  </TextBlock>
+  {#if lang === 'ru'}
+    <TextBlock headerBlock={textData.firstParagraph.headerBlock.ru}>
+      {textData.firstParagraph.textBlock.ru}
+    </TextBlock>
+    <TextBlock headerBlock={textData.firstParagraph.headerBlock.ru}>
+      {textData.firstParagraph.textBlock.ru}
+    </TextBlock>
+    <TextBlock headerBlock={textData.firstParagraph.headerBlock.ru}>
+      {textData.firstParagraph.textBlock.ru}
+    </TextBlock>
+  {:else}
+    <TextBlock headerBlock={textData.firstParagraph.headerBlock.eng}>
+      {textData.firstParagraph.textBlock.eng}
+    </TextBlock>
+    <TextBlock headerBlock={textData.firstParagraph.headerBlock.eng}>
+      {textData.firstParagraph.textBlock.eng}
+    </TextBlock>
+    <TextBlock headerBlock={textData.firstParagraph.headerBlock.eng}>
+      {textData.firstParagraph.textBlock.eng}
+    </TextBlock>
+  {/if}
 </section>
