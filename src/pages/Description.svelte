104aab79a6724504ae0aad408fc4aa18a3d37031
Benjamin <benjamin.sokolov19@gmail.com>
2025-07-02 14:09:59 +0300

    fix: правки

--- src/pages/Description.svelte
+++ src/pages/Description.svelte
@@ -1,9 +1,9 @@
 <script>
-  import { getContext, onDestroy } from 'svelte';
-  import TextBlock from '../components/TextBlock.svelte';
-  import { getTextByLang } from '../utils/getTextByLang';
+  import { getContext, onDestroy } from "svelte";
+  import TextBlock from "../components/TextBlock.svelte";
+  import { getTextByLang } from "../utils/getTextByLang";
 
-  const languageStore = getContext('language');
+  const languageStore = getContext("language");
   let lang;
 
   const unsubscribe = languageStore.subscribe((value) => {
@@ -14,29 +14,40 @@
 </script>
 
 <section class="description">
-  <TextBlock headerBlock={getTextByLang('headerBlockFirst', lang)}>
+  <TextBlock headerBlock={getTextByLang("headerBlockFirst", lang)}>
     <article class="description__text-wrapper">
-      {getTextByLang('textBlockFirst', lang)}
+      {getTextByLang("textBlockFirst", lang)}
     </article>
   </TextBlock>
 </section>
 
 <style>
   .description {
-    /* max-width: 980px;
-    width: 980px; */
-    padding-left: 4px;
-    padding-right: 4px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    max-width: 980px;
+    min-width: 375px;
+
+    overflow-x: hidden;
   }
 
   .description__text-wrapper {
-    padding-left: 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    /* padding-left: 30px; */
     padding-right: 30px;
   }
 
-  /* @media (max-width: 500px) {
-    .description {
-      width: 3px;
+  @media (max-width: 500px) {
+    .description__text-wrapper {
+      width: 375px;
     }
-  } */
+    .description {
+      width: 375px;
+    }
+  }
 </style>
